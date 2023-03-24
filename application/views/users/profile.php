02ed33950f15ccf57cc8e6bd53e2585cf5f979a6
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-03-24 19:14:28 +0300

    generate nav request

--- application/views/users/profile.php
+++ application/views/users/profile.php
@@ -32,7 +32,7 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Profile</li>
+        <li class="active">Account</li>
       </ol>
     </section>
 
