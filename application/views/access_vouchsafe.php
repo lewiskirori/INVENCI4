c10077634c2513bcfb0bbdef00989e5f1b5754ee
Lewis K Ycot <88181151+lewiskirori@users.noreply.github.com>
2023-04-21 13:07:05 -0700

    Update access_vouchsafe.php

--- application/views/access_vouchsafe.php
+++ application/views/access_vouchsafe.php
@@ -252,7 +252,6 @@
         <script src="js/scripts.js"></script>
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
